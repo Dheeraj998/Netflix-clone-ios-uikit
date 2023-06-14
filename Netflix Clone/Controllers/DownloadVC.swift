f915bf2648b1e9592fd015a76800d72a00f51e29
centrric-cntc0023 <cntc0023@centrric.com>
2023-06-14 12:24:15 +0530

    collection view in views added

---
+++ Netflix Clone/Controllers/DownloadVC.swift
@@ -0,0 +1,21 @@
+//
+//  DownloadVC.swift
+//  Netflix Clone
+//
+//  Created by Centrric Developer on 14/06/23.
+//
+
+import UIKit
+
+class DownloadVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+
+
+    }
+    
+
+}
---
+++ Netflix Clone/Controllers/UpComingVC.swift
@@ -0,0 +1,21 @@
+//
+//  UpComingVC.swift
+//  Netflix Clone
+//
+//  Created by Centrric Developer on 14/06/23.
+//
+
+import UIKit
+
+class UpComingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+
+    }
+    
+
+
+}
